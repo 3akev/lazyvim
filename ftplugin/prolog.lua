448edbd6f907664868d592a991ac40b29374e577
Ala Al-Deen Alkhafaji <3akevdev@gmail.com>
2023-11-24 19:53:22 +0100

    add execute with trace for eclipse prolog

--- ftplugin/prolog.lua
+++ ftplugin/prolog.lua
@@ -37,14 +37,14 @@ local function get_goal()
   return goal
 end
 
-local function run_goal()
+local function _run_eclipse(options)
   local goal = get_goal()
 
   if goal == "" then
-    vim.notify("No goal")
+    vim.notify("No goal", vim.log.levels.ERROR)
     return
   end
-  local cmd = string.format("!echo -e '%s' | eclipserun '%s'", goal, vim.fn.expand("%:p"))
+  local cmd = string.format("!echo -e '%s' | eclipserun %s '%s'", goal, options, vim.fn.expand("%:p"))
   local output = vim.fn.execute(cmd)
   output = sanitize_output(output)
   if vim.v.shell_error == 0 then
@@ -54,23 +54,41 @@ local function run_goal()
   end
 end
 
-local function run_goal_all_results()
+local function _run_eclipse_trace()
   local goal = get_goal()
 
   if goal == "" then
-    vim.notify("No goal")
+    vim.notify("No goal", vim.log.levels.ERROR)
     return
   end
-  local cmd = string.format("!echo -e '%s' | eclipserun -a '%s'", goal, vim.fn.expand("%:p"))
-  local output = vim.fn.execute(cmd)
-  output = sanitize_output(output)
-  if vim.v.shell_error == 0 then
-    vim.notify(output, vim.log.levels.INFO)
-  else
-    vim.notify(output, vim.log.levels.ERROR)
-  end
+
+  local file = vim.fn.expand("%:p")
+
+  vim.cmd("split")
+  vim.cmd("resize -12")
+  local win = vim.api.nvim_get_current_win()
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_win_set_buf(win, buf)
+  vim.fn.termopen("eclipse")
+  vim.api.nvim_put({
+    string.format("['%s'].", file),
+    "trace.",
+    goal,
+    "",
+  }, "l", true, true)
+  vim.fn.wait(150, false)
+  vim.cmd("startinsert")
 end
 
 -- keybind run current line/selection as goal
-vim.keymap.set({ "n", "v" }, "<localleader>r", run_goal, { desc = "Run goal" })
-vim.keymap.set({ "n", "v" }, "<localleader>R", run_goal_all_results, { desc = "Run goal (all results)" })
+vim.keymap.set({ "n", "v" }, "<localleader>r", function()
+  _run_eclipse("")
+end, { desc = "Run goal" })
+
+vim.keymap.set({ "n", "v" }, "<localleader>R", function()
+  _run_eclipse("-a")
+end, { desc = "Run goal (all results)" })
+
+vim.keymap.set({ "n", "v" }, "<localleader>t", function()
+  _run_eclipse_trace()
+end, { desc = "Run goal (trace)" })

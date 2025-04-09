c9681f0bca2df3e293101892531c583e931d1f85
Ala Alkhafaji <3akevdev@gmail.com>
2025-04-09 20:12:00 +0200

    update

--- lua/plugins/file-manager.lua
+++ lua/plugins/file-manager.lua
@@ -2,20 +2,6 @@ return {
   -- hijack netrw even when file manager is loaded lazily
   {
     "nvim-neo-tree/neo-tree.nvim",
-    -- init = function()
-    --   vim.api.nvim_create_autocmd("BufEnter", {
-    --     -- make a group to be able to delete it later
-    --     group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
-    --     callback = function()
-    --       local f = vim.fn.expand("%:p")
-    --       if vim.fn.isdirectory(f) ~= 0 then
-    --         vim.cmd("Neotree current dir=" .. f)
-    --         -- neo-tree is loaded now, delete the init autocmd
-    --         vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
-    --       end
-    --     end,
-    --   })
-    -- end,
     opts = {
       filesystem = {
         hijack_netrw_behavior = "disabled",

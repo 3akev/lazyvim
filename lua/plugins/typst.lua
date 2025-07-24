5a2676f6fdfe5861a9752259273f6bcc57f34e90
Ala Alkhafaji <3akevdev@gmail.com>
2025-07-24 19:17:17 +0200

    pin previewed typst file

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -13,6 +13,35 @@ vim.filetype.add({
   },
 })
 
+local function typst_pin()
+  local tinymist_id = nil
+  for _, client in pairs(vim.lsp.get_clients()) do
+    if client.name == "tinymist" then
+      tinymist_id = client.id
+      break
+    end
+  end
+
+  if not tinymist_id then
+    vim.notify("tinymist not running!", vim.log.levels.ERROR)
+    return
+  end
+
+  local client = vim.lsp.get_client_by_id(tinymist_id)
+  if client then
+    client.request("workspace/executeCommand", {
+      command = "tinymist.pinMain",
+      arguments = { vim.api.nvim_buf_get_name(0) },
+    }, function(err)
+      if err then
+        vim.notify("error pinning: " .. err, vim.log.levels.ERROR)
+      else
+        vim.notify("succesfully pinned", vim.log.levels.INFO)
+      end
+    end, 0)
+  end
+end
+
 return {
   {
     "stevearc/conform.nvim",
@@ -64,7 +93,10 @@ return {
     keys = {
       {
         "<localleader>tp",
-        ":TypstPreview<CR>",
+        function()
+          vim.cmd("TypstPreview")
+          typst_pin()
+        end,
         desc = "Start Typst preview",
         mode = { "n" },
       },
@@ -76,7 +108,10 @@ return {
       },
       {
         "<localleader>tt",
-        ":TypstPreviewToggle<CR>",
+        function()
+          vim.cmd("TypstPreviewToggle")
+          typst_pin()
+        end,
         desc = "Toggle Typst preview",
         mode = { "n" },
       },

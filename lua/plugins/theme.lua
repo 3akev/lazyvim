1beaed1540c9d61ad80b3f44876588c38793a4b0
Ala Alkhafaji <3akevdev@gmail.com>
2025-10-07 08:40:55 +0200

    update

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
@@ -16,18 +10,29 @@ return {
       integrations = {
         indent_blankline = {
           enabled = true,
-          scope_color = "lavender", -- catppuccin color (eg. `lavender`) Default: text
+          scope_color = "lavender",
           colored_indent_levels = false,
         },
         mason = true,
         noice = true,
       },
     },
+    specs = {
+      -- {
+      --   "akinsho/bufferline.nvim",
+      --   optional = true,
+      --   opts = function(_, opts)
+      --     if (vim.g.colors_name or ""):find("catppuccin") then
+      --       opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
+      --     end
+      --   end,
+      -- },
+    },
   },
   {
-    "nvim-lualine/lualine.nvim",
+    "LazyVim/LazyVim",
     opts = {
-      theme = "catppuccin",
+      colorscheme = "catppuccin",
     },
   },
 }
